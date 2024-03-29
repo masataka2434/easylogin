ed90fe9d0a936d9437999d11a0740bf603f8b559
masataka2434 <maeda.masataka2434@gmail.com>
2024-03-29 20:21:36 +0900

    Lesson_easylogin

---
+++ src/main/java/jp/con/internous/easylogin/model/mapper/MstUserMapper.java
@@ -0,0 +1,14 @@
+package jp.con.internous.easylogin.model.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import jp.con.internous.easylogin.model.domain.MstUser;
+
+@Mapper
+public interface MstUserMapper {
+	
+	@Select("SELECT * FROM mst_user WHERE user_name = #{userName} AND password = #{password}")
+	MstUser findByUserNameAndPassword( String userName, String password);
+
+}
